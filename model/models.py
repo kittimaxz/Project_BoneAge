5c8856ac9fefaa45d79c2d908a7d312f0e9d9f7f
kittimaxz <76931027+kittimaxz@users.noreply.github.com>
2023-05-07 15:43:01 +0700

    Create models.py

---
+++ model/models.py
@@ -0,0 +1,56 @@
+class BoneAgePredictor(nn.Module):
+    def __init__(self):
+        super(BoneAgePredictor, self).__init__()
+        # Layer 1
+        self.conv1 = nn.Conv2d(1, 16, 3)
+        nn.init.kaiming_normal_(self.conv1.weight)
+        self.batch1 = nn.BatchNorm2d(16)
+        # Layer 2
+        self.conv2 = nn.Conv2d(16, 32, 3)
+        nn.init.kaiming_normal_(self.conv2.weight)
+        self.batch2 = nn.BatchNorm2d(32)
+        # Layer 3
+        self.conv3 = nn.Conv2d(32, 64, 3)
+        nn.init.kaiming_normal_(self.conv3.weight)
+        self.batch3 = nn.BatchNorm2d(64)
+        # Layer 4
+        self.conv4 = nn.Conv2d(64, 128, 3)
+        nn.init.kaiming_normal_(self.conv4.weight)
+        self.batch4 = nn.BatchNorm2d(128)
+        # Layer 5
+        self.conv5 = nn.Conv2d(128, 128, 3)
+        nn.init.kaiming_normal_(self.conv5.weight)
+        self.batch5 = nn.BatchNorm2d(128)
+        # Fully connected
+        self.fc1 = nn.Linear(4609, 68)
+        self.fc2 = nn.Linear(69, 1)
+        #self.fc2 = nn.Linear(64, 1)
+    
+        
+
+    def forward(self, x, m):
+        # Layer 1
+        x = F.relu(self.batch1(self.conv1(x)))
+        x = F.max_pool2d(x, 2)
+        # Layer 2
+        x = F.relu(self.batch2(self.conv2(x)))
+        x = F.max_pool2d(x, 2)
+        # Layer 3
+        x = F.relu(self.batch3(self.conv3(x)))
+        x = F.max_pool2d(x, 2)
+        # Layer 4
+        x = F.relu(self.batch4(self.conv4(x)))
+        x = F.max_pool2d(x, 2)
+        # Layer 5
+        x = F.relu(self.batch5(self.conv5(x)))
+        x = F.max_pool2d(x, 2)
+        
+        # Pooling
+        x = x.view(-1,4608)
+        x = torch.cat((x,m), axis = 1)
+        x = self.fc1(x)
+        x = torch.cat((x,m), axis = 1)
+        x = self.fc2(x)
+        #x = torch.cat((x,m), axis = 1)
+        #x = self.fc3(x)
+        return x
